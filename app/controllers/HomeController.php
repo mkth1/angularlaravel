2fcbb0f237b6ccb54d83ffc401045347aa93b12b
mukesh <Mukesh@Mukesh-MacBook-Pro.local>
2013-12-28 17:11:57 +0200

    made broileplate for angular

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -15,9 +15,6 @@ class HomeController extends BaseController {
 	|
 	*/
 
-	public function showWelcome()
-	{
-		return View::make('hello');
-	}
+	
 
 }
---
+++ app/views/master.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Contacts</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="css/vendors/bootstrap.css" rel="stylesheet">
+    <link href="css/main.css" rel="stylesheet">
+  </head>
+  <body id="home">
+    <section class="container">
+		<!-- row 1: navigation -->
+	    <div class="row">
+	        <!-- the role navigation section is the navbar ,navbar-fixed-top is for full width , navbar-inverse is for color black or inverse color and icon-bar is the icons which comes on the button when u reduce the page size!-->
+	        <nav class="navbar navbar-default navbar-inverse navbar-fixed-top" role="navigation">
+	            <div class="navbar-header">
+	            	<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#collapse" >
+	                    <span class="src-only"></span>   
+	                    <span class="icon-bar"></span>
+	                    <span class="icon-bar"></span>
+	                    <span class="icon-bar"></span>
+	                </button>
+	                <a href="" class="navbar-brand">My Website</a>
+	            </div> 
+	            <div class="collapse navbar-collapse" id="collapse">
+	                <ul class="nav navbar-nav">
+	                    <li><a href="#">Home</a></li>
+	                    <li><a href="#">About</a></li>
+	                    <li><a href="#">Photo Gallery</a></li>
+	                    <li><a href="#">Contact</a></li>  
+	                </ul>
+	            </div>
+	        </nav> 
+	    </div> <!-- navigation end row -->
+
+	<!-- row 2: header 
+    <header class="row">
+    	<div class="jumbotron">Contacts</div>
+    </header> -->
+    
+    <!-- main content -->
+    <div class="row">
+    </div>
+	</section> <!-- container -->
+    
+    <script src="js/vendors/jquery.js" type="text/javascript"></script>
+    <script src="js/vendors/bootstrap.js" type="text/javascript"></script>
+    <script src="js/vendors/angular.js" type="text/javascript"></script>
+    <script src="js/vendors/angular-route.js" type="text/javascript"></script>
+    <script src="js/vendors/angular-resource.js" type="text/javascript"></script>
+  </body>
+</html>
